44f3bac412e934b4adcde02b7faca5a8a50084c6
kaique1605 <kaiqueribeiro.01@hotmail.com>
2019-07-08 22:29:45 -0300

    project base

---
+++ src/main/java/pdsi/com/SIM/SimApplication.java
@@ -0,0 +1,13 @@
+package pdsi.com.SIM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimApplication.class, args);
+	}
+
+}
